2c2507b49f5dc81ba7933b8245c45f01edd7139d
Anantharaman Ramachandran <Ramachandran.Anantharaman@navis.com>
2021-03-10 17:17:37 +0530

    Changes to run training and test seperately for the demo

--- main.py
+++ main.py
@@ -33,6 +33,31 @@ def train_model(filename, desc, req):
 
     plt.show()
 
+def predict(testFileName, title, endpoint):
+    plt.xlabel('Sample No.')
+    plt.ylabel('Value')
+
+    # print("OSLO Block-wise prediction actual numbers...")
+    print(title)
+    plt.title('% Actual(red) Vs Predicted(blue)' + title)
+
+    dataframe = pd.read_csv(testFileName, header=None)
+    data = dataframe.values
+    X, y = data[:, :-1], data[:, -1:]
+
+    t = requests.post(endpoint, json=data.tolist());
+
+    jsonOfRetValues = json.loads(t.text)
+
+    plt.xlim([0, 10])
+    plt.ylim([0, 100])
+
+    x = list(range(0, len(jsonOfRetValues["actuals"])))
+    plt.plot(x, jsonOfRetValues["actuals"], 'r')
+    plt.plot(x, jsonOfRetValues["predictions"], 'b')
+
+    plt.show()
+
 
 if __name__ == '__main__':
     train_model('./resources/total_yard_percentage_scope_4.csv', 'Full yard occupancy percentage - GEBZE',
@@ -42,7 +67,13 @@ if __name__ == '__main__':
     train_model('./resources/total_yard_percentage_scope_5.csv', 'Full yard occupancy percentage - GEMLIK',
                  'http://localhost:5009/api/CreateModelWithTrainingData?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
 
-    predict('./resources/total_yard_percentage_scope_2.csv', 'Full yard occupancy percentage - OPA',
-                'http://localhost:5009/api/CreateModelWithTrainingData?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
+    predict('./resources/test_scope_2.csv', 'Test Full yard occupancy percentage - OPA',
+            'http://localhost:5009/api/PredictUsingModel?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
+
+    predict('./resources/test_scope_4.csv', 'Test Full yard occupancy percentage - GEBZE',
+            'http://localhost:5009/api/PredictUsingModel?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
+
+    predict('./resources/test_scope_5.csv', 'Test Full yard occupancy percentage - GEMLIK',
+                'http://localhost:5009/api/PredictUsingModel?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
