0a7a7d09c1c96e0e081819268dc99fc0dd47063e
Anantharaman Ramachandran <Ramachandran.Anantharaman@navis.com>
2021-03-12 19:02:10 +0530

    Last but one

--- main.py
+++ main.py
@@ -7,73 +7,114 @@ import json
 import requests
 import matplotlib.pyplot as plt
 import pandas as pd
+import tkinter
+from tkinter import *
+from tkinter import messagebox
 
-def train_model(filename, desc, req):
-    plt.xlabel('Sample No.')
-    plt.ylabel('Value')
+import numpy as np
+import ctypes
 
-    # print("OSLO Block-wise prediction actual numbers...")
-    print(desc)
-    plt.title('% Actual(red) Vs Predicted(blue)' + desc)
 
-    dataframe = pd.read_csv(filename, header=None)
-    data = dataframe.values
-    X, y = data[:, :-1], data[:, -1:]
-
-    t = requests.post(req, json=data.tolist());
-
-    jsonOfRetValues = json.loads(t.text)
-
-    plt.xlim([0, 10])
-    plt.ylim([0, 100])
-
-    x = list(range(0, len(jsonOfRetValues["actuals"])))
-    plt.plot(x, jsonOfRetValues["actuals"], 'r')
-    plt.plot(x, jsonOfRetValues["predictions"], 'b')
-
-    plt.show()
-
-def predict(testFileName, title, endpoint):
-    plt.xlabel('Sample No.')
+def train_model(trainingDataFileName, title, endpoint):
+    #plt.xlabel('Sample No.')
     plt.ylabel('Value')
 
     # print("OSLO Block-wise prediction actual numbers...")
     print(title)
-    plt.title('% Actual(red) Vs Predicted(blue)' + title)
+    plt.title(title+" yard occupancy prediction")
 
-    dataframe = pd.read_csv(testFileName, header=None)
+    dataframe = pd.read_csv(trainingDataFileName, header=None)
     data = dataframe.values
     X, y = data[:, :-1], data[:, -1:]
 
-    t = requests.post(endpoint, json=data.tolist());
+    req = requests.post(endpoint, json=data.tolist());
+    if req.status_code == 201:
+        jsonOfRetValues = json.loads(req.text)
+        mape = jsonOfRetValues["mape"]
+        rmse = jsonOfRetValues["rmse"]
+        successMessage = "The model was created with RMSE = "+str(rmse)+" and MAPE = "+str(mape)
+        ctypes.windll.user32.MessageBoxW(0, successMessage, "Success!", 1)
+    elif req.status_code == 400:
+        ctypes.windll.user32.MessageBoxW(0, "The model could not be created!", "Error!", 1)
+        return;
 
-    jsonOfRetValues = json.loads(t.text)
+def predict(referenceDataFile, inputForPredictionFile, title, endpoint):
+    #plt.xlabel('Sample No.')
+    plt.ylabel('Value')
+
+    print(title)
+    #plt.title('% Actual(red) Vs Prediction(blue)' + title)
+    plt.title(title + " yard occupancy prediction")
+
+    dataframe = pd.read_csv(referenceDataFile, header=None)
+    actualData = dataframe.values
+    X_actuals, y_actuals = actualData[:, :-1], (actualData[:, -1:]).tolist()
+
+    dataframe = pd.read_csv(inputForPredictionFile, header=None)
+    data = dataframe.values
+    X, y = data[:, :-1], data[:, -1:]
+
+    req = requests.post(endpoint, json=data.tolist());
+
+    predictionsResp = json.loads(req.text)
 
     plt.xlim([0, 10])
     plt.ylim([0, 100])
 
-    x = list(range(0, len(jsonOfRetValues["actuals"])))
-    plt.plot(x, jsonOfRetValues["actuals"], 'r')
-    plt.plot(x, jsonOfRetValues["predictions"], 'b')
+    x = list(range(0, len(y_actuals)))
+    plt.plot(x, y_actuals, 'r', label='Actuals')
+    plt.plot(x, predictionsResp["predictions"], 'b', label='Predictions')
+
+    plt.plot(x, y_actuals, 'xr')
+    plt.plot(x, predictionsResp["predictions"], 'xb')
+
+    plt.legend()
+    mapeText = "MAPE = " + "{:.5f}".format(predictionsResp["mape"])
+    rmseText = "RMSE = " + "{:.5f}".format(predictionsResp["rmse"])
+    plt.text(-1.2, -5.5, mapeText+"\n"+rmseText, bbox=dict(facecolor='blue', alpha=0.5))
+
+
+    for i in range(0,10):
+        plt.annotate(i,(x[i], y_actuals[i][0]),textcoords="offset points", xytext=(0, 10), ha='center', color='r');
+        plt.annotate(i, (x[i], predictionsResp["predictions"][i]), textcoords="offset points", xytext=(0, -20), ha='center', color='b');
+
+    ax = plt.gca()
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.xaxis.set_ticks([])
+
+    d = {'Number':range(1,11),'Actual':y_actuals, 'Prediction':predictionsResp["predictions"]}
+    df = pd.DataFrame(d)
+    table = plt.table(cellText=df.values, colLabels=df.columns, loc='bottom', cellLoc='center')
+
+    plt.tight_layout()
+    mng = plt.get_current_fig_manager()
+    mng.window.state('zoomed')
+
+
 
     plt.show()
 
 
 if __name__ == '__main__':
-    train_model('./resources/total_yard_percentage_scope_4.csv', 'Full yard occupancy percentage - GEBZE',
-                'http://localhost:5009/api/CreateModelWithTrainingData?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
-    train_model('./resources/total_yard_percentage_scope_2.csv', 'Full yard occupancy percentage - OPA',
-                'http://localhost:5009/api/CreateModelWithTrainingData?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
-    train_model('./resources/total_yard_percentage_scope_5.csv', 'Full yard occupancy percentage - GEMLIK',
-                 'http://localhost:5009/api/CreateModelWithTrainingData?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
+    train_model('./resources/total_yard_percentage_scope_GEB.csv', 'Full yard occupancy percentage - GEBZE',
+                'http://localhost:5007/api/CreateModelWithTrainingData?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
 
-    predict('./resources/test_scope_2.csv', 'Test Full yard occupancy percentage - OPA',
-            'http://localhost:5009/api/PredictUsingModel?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
+    predict('./resources/test_scope_GEB_with_labels.csv', './resources/test_scope_GEB_without_labels.csv',
+            'GEBZE',
+            'http://localhost:5007/api/PredictUsingModel?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
 
-    predict('./resources/test_scope_4.csv', 'Test Full yard occupancy percentage - GEBZE',
-            'http://localhost:5009/api/PredictUsingModel?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
+    train_model('./resources/total_yard_percentage_scope_OPA.csv', 'Full yard occupancy percentage - OPA',
+                'http://localhost:5007/api/CreateModelWithTrainingData?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
 
-    predict('./resources/test_scope_5.csv', 'Test Full yard occupancy percentage - GEMLIK',
-                'http://localhost:5009/api/PredictUsingModel?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
+    predict('./resources/test_scope_OPA_with_labels.csv', './resources/test_scope_OPA_without_labels.csv',
+            'OPA',
+            'http://localhost:5007/api/PredictUsingModel?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
+
+    train_model('./resources/total_yard_percentage_scope_GEM.csv', 'Full yard occupancy percentage - GEMLIK',
+                'http://localhost:5007/api/CreateModelWithTrainingData?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
+
+    predict('./resources/test_scope_GEM_with_labels.csv', './resources/test_scope_GEM_without_labels.csv',
+            'GEMLIK',
+            'http://localhost:5007/api/PredictUsingModel?consumerId=FULL_YARD_PREDICTION_PERCENTAGE')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
